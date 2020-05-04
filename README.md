c287617f206f8a5317d7e499a29e64fc7ff9dc3c
Anton Reindl <toni.reindl@web.de>
2020-05-04 16:52:44 +0200

    crawling done

--- README.md
+++ README.md
@@ -1,5 +1,28 @@
-# notdienst_api
+# Apotheken Notdienst Crawler
+
+### A simple crawler in Node.js to build a simple API to get the current emergency pharmacy for locations in Germany directly from [BLAK](http://www.lak-bayern.notdienst-portal.de/blakportal/).
+
+## Introduction
+
+This repository contains code for a web crawler that scrapes a website. The API can be pubslished using a Serverless function at hosters like [Netlify](https://netlify.com).
+
+## Installation
+
+Clone the repository
+Install dependencies: `npm install`
+Navigate into the src directory and run the main thread: `node index.js`
+
+This project was built with the following:
+
+- Node: A JavaScript runtime built on Chrome's V8 JavaScript engine.
+- Axios: A promised based HTTP client for the browser and Node.js.
+- Cheerio: A lightweight implementation of jQuery which gives us access to the DOM on the server.
+- Netlify Functions for Serverless Deployment
 
 ## License
 
 This project is licensed under the [MIT license](https://opensource.org/licenses/MIT).
+
+## Credits
+
+Thanks to [this](https://blog.logrocket.com/how-to-build-a-web-crawler-with-node/) tutorial by [Logrocket](https://logrocket.com).
---
+++ index.js
@@ -0,0 +1,52 @@
+"use strict";
+const express = require("express");
+const path = require("path");
+const serverless = require("serverless-http");
+const app = express();
+const bodyParser = require("body-parser");
+const crawler = require("./crawler");
+
+/// Crawling Target
+const url =
+  "http://www.lak-bayern.notdienst-portal.de/blakportal/schnellsuche/ergebnis";
+
+let params = {
+  lat: 47.52838000000001,
+  lon: 11.1113161,
+  date: Date.now(),
+  location: "Farchant",
+};
+
+let result = {};
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("<h1>Notdienst API für deutsche Apotheken</h1>");
+  res.end();
+});
+router.get("/notdienst", (req, res) => {
+  // Get Results by crawling and parsing
+  this.result = crawler
+    .getResults(url, params)
+    .then((res) => {
+      return res;
+    })
+    .catch((err) => {
+      console.error(err);
+      console.error("Crawling/Parsing failed");
+      return err;
+    });
+
+  res.json(result);
+});
+
+app.use(bodyParser.json());
+app.use("/.netlify/functions/server", router); // path must route to lambda
+app.use("/", (req, res) => {
+  res.json(this.result);
+});
+
+module.exports = app;
+module.exports.handler = serverless(app);
