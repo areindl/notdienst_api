22ce7c1c55edc08a67fb713cf365aca8e2f84d8a
Anton Reindl <toni.reindl@web.de>
2020-05-04 20:44:21 +0200

    added crawler module

--- functions/server.js
+++ functions/server.js
@@ -1,8 +1,35 @@
+const crawler = require("../crawler");
+
 exports.handler = async (event) => {
   const lat = event.queryStringParameters.name || 45;
 
-  return {
-    statusCode: 200,
-    body: `Hello ${subject}`,
+  /// Crawling Target
+  const url =
+    "http://www.lak-bayern.notdienst-portal.de/blakportal/schnellsuche/ergebnis";
+
+  let params = {
+    lat: 47.52838000000001,
+    lon: 11.1113161,
+    date: Date.now(),
+    location: "Farchant",
   };
+
+  let result = {};
+
+  result = crawler
+    .getResults(url, params)
+    .then((res) => {
+      return {
+        statusCode: 200,
+        body: res,
+      };
+    })
+    .catch((err) => {
+      console.error(err);
+      console.error("Crawling/Parsing failed");
+      return {
+        statusCode: 500,
+        body: "Could not load API",
+      };
+    });
 };
